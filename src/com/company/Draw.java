56b3cc61875493cfdcc2b09af6ae2b9a7796c15f
Elizabeth Aufzien <elizabeth@rocketreach.co>
2023-07-12 10:20:18 -0400

    refactor close action out of draw

--- src/com/company/Draw.java
+++ src/com/company/Draw.java
@@ -1114,37 +1114,3 @@ public class Draw implements ActionListener, MouseListener, MouseMotionListener,
 }
 
 
-/**
- * Aligns functionality of closing window with red X in top corner and quitting
- * from the file menu.
- */
-class CloseAction extends AbstractAction {
-  private final JFrame mainFrame;
-
-
-  /**
-   * Todo: insert javadoc comment.
-   *
-   * @param mainFrame
-   */
-  public CloseAction(JFrame mainFrame) {
-    super("Exit");
-    putValue(MNEMONIC_KEY, KeyEvent.VK_X);
-    this.mainFrame = mainFrame;
-  }
-
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    confirmClosing();
-  }
-
-  public void confirmClosing() {
-    int confirmed = JOptionPane.showConfirmDialog(mainFrame,
-        "Are you sure you want to quit?", "Confirm quit",
-        JOptionPane.YES_NO_OPTION);
-    if (confirmed == JOptionPane.YES_OPTION) {
-      // clean up code
-      System.exit(0);
-    }
-  }
-}
